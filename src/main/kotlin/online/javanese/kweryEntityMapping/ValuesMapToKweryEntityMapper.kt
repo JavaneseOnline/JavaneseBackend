7b826deb169728b8e88f04cc915bb833d7e803a0
Miha_x64 <mihlo1996@rambler.ru>
2017-10-19 00:45:31 +0300

    name transform support in ValuesMapToKweryEntityMapper

--- src/main/kotlin/online/javanese/kweryEntityMapping/ValuesMapToKweryEntityMapper.kt
+++ src/main/kotlin/online/javanese/kweryEntityMapping/ValuesMapToKweryEntityMapper.kt
@@ -4,7 +4,8 @@ import com.github.andrewoma.kwery.mapper.Table
 import org.jetbrains.ktor.util.ValuesMap
 
 class ValuesMapToKweryEntityMapper<out T : Any, ID>(
-        private val table: Table<T, ID>
+        private val table: Table<T, ID>,
+        private val nameTransform: (String) -> String = { it }
 ) : (ValuesMap) -> T {
 
     // TODO validation: return an instance of a sealed class ( success | error )
@@ -12,6 +13,6 @@ class ValuesMapToKweryEntityMapper<out T : Any, ID>(
     private val valueFactory = ValueFactory(table)
 
     override fun invoke(map: ValuesMap): T =
-            table.create(valueFactory.from(ValuesMapAsSource(map)))
+            table.create(valueFactory.from(ValuesMapAsSource(map, nameTransform)))
 
 }
