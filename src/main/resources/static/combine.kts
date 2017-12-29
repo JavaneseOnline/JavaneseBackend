f387efb597ce33de2fdea132f6af76d9c388643c
Miha_x64 <mihlo1996@rambler.ru>
2017-12-29 23:47:28 +0300

    Running SCSS + CSSO automatically

--- src/main/resources/static/combine.kts
+++ src/main/resources/static/combine.kts
@@ -2,44 +2,75 @@ import java.io.File
 import java.io.OutputStreamWriter
 import java.net.URL
 
-val main = "js/vue_zepto_mdl_dialog_highlight_trace_scroll_unfocus_tabs_form"
-val sandbox = "sandbox/codemirror_clike_sandbox"
+
 val nl = charArrayOf('\n')
 
+val mainScript = "js/vue_zepto_mdl_dialog_highlight_trace_scroll_unfocus_tabs_form"
+val sandboxScript = "sandbox/codemirror_clike_sandbox"
 
-jsFile(main).assertNotExists().writer().use {
-    appendFrom(URL("https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.25/vue.min.js"), it) //todo: upgrade
-    appendFrom(URL("http://zeptojs.com/zepto.min.js"), it)
-    appendFrom(URL("https://raw.githubusercontent.com/madrobby/zepto/master/src/fx.js"), it)
-    appendFrom(URL("https://raw.githubusercontent.com/madrobby/zepto/master/src/fx_methods.js"), it)
+val mainStyle = "css/main"
+val codeMirrorStyle = "sandbox/codemirror_ambiance"
 
-    it.write(
-            URL("https://raw.githubusercontent.com/suprMax/ZeptoScroll/master/static/zepto.scroll.js")
-                    .openConnection().getInputStream().reader().readText()
-                    .replace("$.os.android ? 1 : 0", "0") // we're not using Zepto.detect
-    )
-    it.write(nl)
 
-    appendFrom(URL("https://code.getmdl.io/1.1.3/material.min.js"), it) // TODO: upgrade whole MDL
-    appendFrom(URL("https://raw.githubusercontent.com/GoogleChrome/dialog-polyfill/master/dialog-polyfill.js"), it)
-    appendFrom(File("js/trace.js"), it)
-    appendFrom(File("js/highlight.pack.js"), it)
-    appendFrom(File("js/scroll_unfocus_tabs.js"), it)
-    appendFrom(File("js/form.js"), it)
+combineAndUglifyMain(mainScript)
+combineAndUglifySandbox(sandboxScript)
+scssAndCsso(mainStyle)
+scssAndCsso(codeMirrorStyle)
+
+
+fun combineAndUglifyMain(fileName: String) {
+    jsFile(fileName).assertNotExists().writer().use {
+                appendFrom(URL("https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.25/vue.min.js"), it) //todo: upgrade
+                appendFrom(URL("http://zeptojs.com/zepto.min.js"), it)
+                appendFrom(URL("https://raw.githubusercontent.com/madrobby/zepto/master/src/fx.js"), it)
+                appendFrom(URL("https://raw.githubusercontent.com/madrobby/zepto/master/src/fx_methods.js"), it)
+
+                it.write(
+                        URL("https://raw.githubusercontent.com/suprMax/ZeptoScroll/master/static/zepto.scroll.js")
+                                .openConnection().getInputStream().reader().readText()
+                                .replace("$.os.android ? 1 : 0", "0") // we're not using Zepto.detect
+                )
+                it.write(nl)
+
+                appendFrom(URL("https://code.getmdl.io/1.1.3/material.min.js"), it) // TODO: upgrade whole MDL
+                appendFrom(URL("https://raw.githubusercontent.com/GoogleChrome/dialog-polyfill/master/dialog-polyfill.js"), it)
+                appendFrom(File("js/trace.js"), it)
+                appendFrom(File("js/highlight.pack.js"), it)
+                appendFrom(File("js/scroll_unfocus_tabs.js"), it)
+                appendFrom(File("js/form.js"), it)
+            }
+    minifyJs(fileName)
 }
-minify(main)
 
-
-jsFile(sandbox).assertNotExists().writer().use {
-    appendFrom(URL("https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.33.0/codemirror.min.js"), it)
-    appendFrom(URL("https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.33.0/mode/clike/clike.min.js"), it)
-    appendFrom(File("sandbox/sandbox.js"), it)
+fun combineAndUglifySandbox(fileName: String) {
+    jsFile(fileName).assertNotExists().writer().use {
+                appendFrom(URL("https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.33.0/codemirror.min.js"), it)
+                appendFrom(URL("https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.33.0/mode/clike/clike.min.js"), it)
+                appendFrom(File("sandbox/sandbox.js"), it)
+            }
+    minifyJs(fileName)
 }
-minify(sandbox)
-
 
 fun jsFile(name: String) = File("$name.js")
 fun minJsFile(name: String) = File("$name.min.js")
+
+fun scssFile(name: String) = File("$name.scss")
+fun cssFile(name: String) = File("$name.css")
+fun cssMapFile(name: String) = File("$name.css.map")
+fun minCssFile(name: String) = File("$name.min.css")
+
+fun scssAndCsso(fileName: String) {
+    val cssFile = cssFile(fileName).assertNotExists()
+    val minCssFile = minCssFile(fileName)
+    println("Running SCSS...")
+    check(Runtime.getRuntime().exec(arrayOf("scss", scssFile(fileName).path, cssFile.path)).waitFor() == 0)
+    check(cssMapFile(fileName).delete())
+    println("Running CSSO...")
+    check(Runtime.getRuntime().exec(arrayOf("csso", cssFile.path, minCssFile.path)).waitFor() == 0)
+    check(cssFile.delete())
+    print(minCssFile.path)
+    println(" has been created.")
+}
 
 fun File.assertNotExists() = apply { check(!exists()) }
 
@@ -57,10 +88,10 @@ fun appendFrom(file: File, output: OutputStreamWriter) {
     println(" Done.")
 }
 
-fun minify(name: String) {
+fun minifyJs(name: String) {
     val src = jsFile(name)
 
-    println("minification...")
+    println("Running UglifyJS...")
     val minified = minJsFile(name)
     if (minified.exists() && minified.isFile) {
         check(minified.delete())
@@ -68,11 +99,7 @@ fun minify(name: String) {
     check(Runtime.getRuntime().exec(arrayOf("uglifyjs", "-o", minified.path, src.path)).waitFor() == 0)
 
     print(minified.name)
-    print(" has been created. Press Enter to remove temporary ")
-    print(src.name)
-    println("...")
-
-    System.`in`.read()
+    println(" has been created.")
 
     src.delete()
 }
