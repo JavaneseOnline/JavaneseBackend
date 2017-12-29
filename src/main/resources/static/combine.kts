100b9971292723b3716217e7aeb9c066bdf56106
Miha_x64 <mihlo1996@rambler.ru>
2017-12-29 18:56:01 +0300

    generalized JS combining script

--- src/main/resources/static/combine.kts
+++ src/main/resources/static/combine.kts
@@ -2,12 +2,11 @@ import java.io.File
 import java.io.OutputStreamWriter
 import java.net.URL
 
-val fileName = "vue_zepto_mdl_dialog_highlight_trace_scroll_unfocus_tabs_form"
+val main = "js/vue_zepto_mdl_dialog_highlight_trace_scroll_unfocus_tabs_form"
 val nl = charArrayOf('\n')
 
-val combined = File("$fileName.js")
-check(!combined.exists())
-combined.writer().use {
+
+jsFile(main).also { check(!it.exists()) }.writer().use {
     appendFrom(URL("https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.25/vue.min.js"), it) //todo: upgrade
     appendFrom(URL("http://zeptojs.com/zepto.min.js"), it)
     appendFrom(URL("https://raw.githubusercontent.com/madrobby/zepto/master/src/fx.js"), it)
@@ -22,28 +21,16 @@ combined.writer().use {
 
     appendFrom(URL("https://code.getmdl.io/1.1.3/material.min.js"), it) // TODO: upgrade whole MDL
     appendFrom(URL("https://raw.githubusercontent.com/GoogleChrome/dialog-polyfill/master/dialog-polyfill.js"), it)
-    appendFrom(File("trace.js"), it)
-    appendFrom(File("highlight.pack.js"), it)
-    appendFrom(File("scroll_unfocus_tabs.js"), it)
-    appendFrom(File("form.js"), it)
+    appendFrom(File("js/trace.js"), it)
+    appendFrom(File("js/highlight.pack.js"), it)
+    appendFrom(File("js/scroll_unfocus_tabs.js"), it)
+    appendFrom(File("js/form.js"), it)
 }
+minify(main)
 
-println("minification...")
-val minified = File("$fileName.min.js")
-if (minified.exists() && minified.isFile) {
-    check(minified.delete())
-}
-check(Runtime.getRuntime().exec(arrayOf("uglifyjs", "-o", minified.name, combined.name)).waitFor() == 0)
 
-print(minified.name)
-print(" has been created. Press Enter to remove temporary ")
-print(combined.name)
-println("...")
-
-System.`in`.read()
-
-combined.delete()
-
+fun jsFile(name: String) = File("$name.js")
+fun minJsFile(name: String) = File("$name.min.js")
 
 fun appendFrom(address: URL, output: OutputStreamWriter) {
     print("Downloading from $address...")
@@ -57,4 +44,24 @@ fun appendFrom(file: File, output: OutputStreamWriter) {
     file.reader().use { it.copyTo(output) }
     output.write(nl)
     println(" Done.")
+}
+
+fun minify(name: String) {
+    val src = jsFile(name)
+
+    println("minification...")
+    val minified = minJsFile(name)
+    if (minified.exists() && minified.isFile) {
+        check(minified.delete())
+    }
+    check(Runtime.getRuntime().exec(arrayOf("uglifyjs", "-o", minified.path, src.path)).waitFor() == 0)
+
+    print(minified.name)
+    print(" has been created. Press Enter to remove temporary ")
+    print(src.name)
+    println("...")
+
+    System.`in`.read()
+
+    src.delete()
 }
